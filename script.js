02577c169108afc560c708ab563147b3c3302440
Davron Gaziev <davron>
2023-02-12 19:28:23 +0500

    second commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // console.log(document.head);
 // console.log(document.documentElement);
-console.log(document.body.childNodes);
+// console.log(document.body.childNodes);
 // console.log(document.body.firstChild);
 // console.log(document.body.lastChild);
 // console.log(document.querySelector('#current').parentNode.parentNode);
@@ -10,9 +10,36 @@ console.log(document.body.childNodes);
 // console.log(document.querySelector('[data-current="3"]').previousSibling);
 // console.log(document.querySelector('[data-current="3"]').nextElementSibling);
 
-for (let node of document.body.childNodes) {
-    if (node.nodeName == '#text') {
-        continue;
-    }
-    // console.log(node);
-}
+// for (let node of document.body.childNodes) {
+//     if (node.nodeName == '#text') {
+//         continue;
+//     }
+//     console.log(node);
+// }
+
+// console.log(document.body);
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastElementChild);
+
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('#current').parentElement);
+// console.log(document.querySelector('#current').parentElement.parentElement);
+
+// console.log(document.querySelector('[data-current="3"]').nextSibling);
+// console.log(document.querySelector('[data-current="3"]').previousSibling);
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+// console.log(document.querySelector('[data-current="3"]').previousElementSibling);
+
+
+// for (let node of document.body.childNodes) {
+//     if (node.nodeName == "#text") {
+//         continue;
+//     }
+//     console.log(node);
+// }
+ 
